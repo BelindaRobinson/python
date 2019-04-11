676b8e2110f2f12fcb225da5db5157d23355e390
unknown <binnybeez@hotmail.com>
2019-04-12 11:49:47 +1200

    help from jess

--- p2.1.py
+++ p2.1.py
@@ -2,6 +2,7 @@
 import sys
 import csv
 import re
+import operator
 
 #diplays a menu to the user which gives them the option to where they would like to go 
 def menu():
@@ -51,7 +52,6 @@ def add():
 
 #takes user input and searchs for the csv file for that name, if found there is an option to remove the player 
 def remove():
-##    NEED TO REMOVE SEARCH DATA
     with open ('info.txt', 'r') as datafile:
         name = input("enter name to search")
         datafilereader = csv.reader(datafile)
@@ -62,12 +62,14 @@ def remove():
                     a = 'yes'                    
                     inputremove = input("would you like to remove this player, yes or no?")                    
                     if a in inputremove:
-                        with open ('info.txt', 'w') as datafile:                            
-                            del(row)                            
-                            print("player removed")
-                    else:
-                        print("please try again")
-                        menu()
+                        with open ('info.txt', 'w') as datafile:
+                            if field in row:
+                                del(row)
+                                print("player removed")
+                            else:
+                                print("please try again")
+                                menu()
+                                
                         
     datafile.close()
 
@@ -81,23 +83,41 @@ def update():
                 if field == name:
                     print(row)
                     a = 'yes'                    
-                    inputremove = input("would you like to update this player, yes or no?")                    
-                    if a in inputremove:
-                        name = input("please enter your name")
-                        email = inputcharacter("please enter you email address")    
-                        phone = inputint("please enter your mobile number")
-                        character = input("please enter your characters class")       
-                        group = input("please enter your group name")
-                        with open ('info.txt', 'w') as datafile:
-                            datafilewriter = csv.writer(datafile)
-                            datafilewriter.writerow([name, email, phone, character, group])
-                    else:
-                        print("please try again")
-                        menu()                        
+                    inputremove = input("would you like to update this player, yes or no?")
+                    with open('info.txt', 'a') as datafile:
+                        if a in inputremove:
+                            name = input("please enter your name")
+                            email = inputcharacter("please enter you email address")    
+                            phone = inputint("please enter your mobile number")
+                            character = input("please enter your characters class")       
+                            group = input("please enter your group name")
+                            with open ('info.txt', 'a') as datafile:
+                                datafilewriter = csv.writer(datafile)
+                                datafilewriter.writerow([name, email, phone, character, group])
+                        else:
+                            print("please try again")
+                            menu()                        
 
     datafile.close()
-    
-#def sort():
+    #info = f.readline()
+    # = ["a","b"]
+    #finds this person
+    #
+def sort():
+    a = 'yes'
+    sortfile = input("would you like to sort the file")
+    with open('info.txt', 'r') as datafile:
+        if a in sortfile:
+            datafilereader = csv.reader(datafile)
+            info = []        
+            for row in datafilereader:
+                info.append(row)
+                sortedlist = sorted(info)
+                print(sortedlist)
+#           split now to get each field           
+            
+#             sort = sorted(datafile)
+#             print(sort)
     
 loop=True      
   
